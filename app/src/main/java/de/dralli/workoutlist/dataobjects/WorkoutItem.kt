92da36b3427a5846171b91ad9f22140690122cee
DrAlli (Marius Martin) <35598714+DrAlliteration@users.noreply.github.com>
2022-11-02 10:32:49 +0100

    feat: defined datastructure of WorkoutItem

--- app/src/main/java/de/dralli/workoutlist/dataobjects/WorkoutItem.kt
+++ app/src/main/java/de/dralli/workoutlist/dataobjects/WorkoutItem.kt
@@ -1,5 +1,19 @@
 package de.dralli.workoutlist.dataobjects
 
-class WorkoutItem(){
-
+data class WorkoutItem(
+    var name: String?,
+    var minReps: Int?,
+    var maxReps: Int?,
+    var sets: Int?,
+    var weight: Int?,
+    var done: Boolean
+) {
+    constructor(attributes: Map<String, Any>) : this(null, null, null, null, null, false){
+        name = attributes.get("name") as String?
+        minReps = attributes.get("minReps") as Int?
+        maxReps = attributes.get("maxReps") as Int?
+        sets = attributes.get("sets") as Int?
+        weight = attributes.get("weight") as Int?
+        done = false
+    }
 }
