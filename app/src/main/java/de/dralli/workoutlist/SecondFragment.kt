ab3e3a13a4ba60d559b0644c4b05ccfba857b660
DrAlli (Marius Martin) <35598714+DrAlliteration@users.noreply.github.com>
2022-11-30 11:04:09 +0100

    feat: Added WorkoutListAdapter to SecondFragment

--- app/src/main/java/de/dralli/workoutlist/SecondFragment.kt
+++ app/src/main/java/de/dralli/workoutlist/SecondFragment.kt
@@ -1,12 +1,16 @@
 package de.dralli.workoutlist
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
+import android.widget.ListView
+import androidx.fragment.app.Fragment
 import de.dralli.workoutlist.databinding.FragmentSecondBinding
+import de.dralli.workoutlist.datacontrollers.WorkoutListController
+import de.dralli.workoutlist.dataobjects.WorkoutItem
+import de.dralli.workoutlist.dataobjects.WorkoutList
+import de.dralli.workoutlist.listadapters.WorkoutItemAdapter
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -14,31 +18,39 @@ import de.dralli.workoutlist.databinding.FragmentSecondBinding
 class SecondFragment : Fragment() {
 
     private var _binding: FragmentSecondBinding? = null
+    private lateinit var listView : ListView
+    var workoutListController: WorkoutListController? = null
+    var workoutList: WorkoutList? = null
+
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
 
-    override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
+    private fun initWorkoutList(parameters: Bundle?) {
+        workoutListController = WorkoutListController.getInstance()
+        val index = parameters?.getInt("WorkoutListIndex")
+        workoutList = workoutListController?.getWorkoutList(index)
+    }
 
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         _binding = FragmentSecondBinding.inflate(inflater, container, false)
+        initWorkoutList(this.arguments)
         return binding.root
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        binding.buttonSecond.setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
-        }
+        listView = view.findViewById(R.id.workout_items)
+        renderList()
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun renderList(){
+        listView.adapter = WorkoutItemAdapter(listView.context, workoutList?.workoutItems ?: ArrayList<WorkoutItem>())
     }
 }
